3a6eb47cc0bf422fe5fd9edd1d4c9c138bad835e
pluvivanto <140719273+pluvivanto@users.noreply.github.com>
2024-03-25 22:14:09 +0900

    Initial Spring Initializer Commit

---
+++ src/main/java/com/chesschess/gifchesschess/GifchesschessApplication.java
@@ -0,0 +1,13 @@
+package com.chesschess.gifchesschess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifchesschessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifchesschessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesschess/gifchesschess/GifchesschessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesschess.gifchesschess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifchesschessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
