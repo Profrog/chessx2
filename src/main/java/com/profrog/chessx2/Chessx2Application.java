a2936fe65814725dfa69c999796bf75114fc4a7e
mingyu <ache159@naver.com>
2024-08-04 23:27:16 +0900

    frontv01

--- src/main/java/com/profrog/chessx2/Chessx2Application.java
+++ src/main/java/com/profrog/chessx2/Chessx2Application.java
@@ -3,24 +3,14 @@ package com.profrog.chessx2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
 public class Chessx2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Chessx2Application.class, args);
-	}
-
-	@GetMapping(value = "/")
-	public String doGetHelloWorld() {
-		return "Hello World";
-	}
-
-	@GetMapping(value = "/chessx2")
-	public String doGetHelloWorldDemo() {
-		return "Hello World (chessx2)";
 	}
 }
